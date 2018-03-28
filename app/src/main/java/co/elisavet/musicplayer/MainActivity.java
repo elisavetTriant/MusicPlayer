926b6b75206e16744e5b69c15a7373c29a85b3ad
Elisavet Triantafyllopoulou <elitriant@gmail.com>
2018-03-28 22:17:48 +0300

    Updated Activity to request permission READ_EXTERNAL_STORAGE

--- app/src/main/java/co/elisavet/musicplayer/MainActivity.java
+++ app/src/main/java/co/elisavet/musicplayer/MainActivity.java
@@ -1,29 +1,60 @@
 package co.elisavet.musicplayer;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
 import java.io.File;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
 
-    ArrayList<Audio> libraryAudioList;
+    private ArrayList<Audio> libraryAudioList;
+    private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 0;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = findViewById(R.id.main_layout);
 
+        showMusicContents();
+
+    }
+
+    private void showMusicContents() {
+        // BEGIN_INCLUDE(displayMusic)
+        // Check if the Read External Storage permission has been granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            Snackbar.make(mLayout,
+                    "Read External Storage permission is available. Loading music...",
+                    Snackbar.LENGTH_SHORT).show();
+            displayMusic();
+        } else {
+            // Permission is missing and must be requested.
+            requestReadExternalStoragePermission();
+        }
+        // END_INCLUDE(startCamera)
+    }
+
+    private void displayMusic(){
         ListView listView = (ListView) findViewById(R.id.library_song_list);
 
         libraryAudioList = loadAudio();
@@ -44,8 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 playMedia(selectedAudio.getData());
             }
         });
-
-
     }
 
     //https://www.sitepoint.com/a-step-by-step-guide-to-building-an-android-audio-player-app/
@@ -76,8 +105,63 @@ public class MainActivity extends AppCompatActivity {
         return audioList;
     }
 
+
+
+    //https://github.com/googlesamples/android-RuntimePermissionsBasic/blob/master/Application/src/main/java/com/example/android/basicpermissions/MainActivity.java
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        if (requestCode == PERMISSION_REQUEST_READ_EXTERNAL_STORAGE) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                Snackbar.make(mLayout, "Read external storage permission was granted. Loading Music.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                displayMusic();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(mLayout, "Read external storage permission was denied. Unable to load music.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+        // END_INCLUDE(onRequestPermissionsResult)
+    }
+
+    /**
+     * Requests the {@link android.Manifest.permission#READ_EXTERNAL_STORAGE} permission.
+     * If an additional rationale should be displayed, the user has to launch the request from
+     * a SnackBar that includes additional information.
+     */
+    private void requestReadExternalStoragePermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with cda button to request the missing permission.
+            Snackbar.make(mLayout, "In order to load the music to the library you need to accept the permission!",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(mLayout, "<b>External storage could not be opened.</b>\nThis occurs when the external storage is not available or if the system has denied access (for example when external storage access has been disabled).", Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+        }
+    }
+
     //https://developer.android.com/guide/components/intents-common.html#Music
-    public void playMedia(String file) {
+    private void playMedia(String file) {
         Intent intent = new Intent();
         intent.setAction(android.content.Intent.ACTION_VIEW);
         File fileToPlay = new File(file);
